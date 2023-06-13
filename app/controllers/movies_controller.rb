e14ef8a9c8f3cbf12e82ca857c876b1be55e01b2
Anshul Agrawal <anagrawal@bestpeers.com>
2023-06-13 12:18:20 +0530

    Added movie all action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,5 +7,39 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
-  
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def new 
+    @movie = Movie.new()
+  end
+
+  def create
+    @movie = Movie.create(movie_params)
+
+      if @movie.save
+        redirect_to @movie
+      else
+        render :new
+      end
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+
+      if @movie.update(movie_params)
+        redirect_to @movie
+      else
+        render :edit, status: :unproccessable_entity
+      end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :rating, :description, :release_date)
+  end 
+
 end
