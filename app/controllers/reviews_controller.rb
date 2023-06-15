1c4dff1b6c8e1d0d72aa715a1151c3efdb0f22e7
Anshul Agrawal <anagrawal@bestpeers.com>
2023-06-15 16:46:02 +0530

    Added index action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,29 @@
 class ReviewsController < ApplicationController
 
   def index
-    # @movie = Movie.find(params[:movie_id])
-    # @reviews = @movie.reviews.all
+    @movie = Movie.find(params[:movie_id])
+    @reviews = @movie.reviews.all
   end
 
+  def new 
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.new
+  end
+
+  def create
+  
+    @movie = Movie.find(params[:movie_id])
+    @review = current_user.reviews.create(review_params)
+    if @movie.save
+      redirect_to @movie
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:body,:star,:movie_id,:user_id)
+  end
 end
