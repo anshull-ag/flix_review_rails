1e3f158f7f485eee2ad9b145ad5f8ea563fd4f34
Anshul Agrawal <anagrawal@bestpeers.com>
2023-06-20 17:10:36 +0530

    Added upcoming movie feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   root to: "movies#index"
+  get '/movies/upcoming_movies', to: 'movies#upcoming_movies'
+  
   resources :movies do
     resources :reviews
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_15_075751) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_20_093407) do
   create_table "movies", force: :cascade do |t|
     t.string "name"
     t.string "rating"
     t.string "description"
-    t.string "release_date"
+    t.date "released_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
