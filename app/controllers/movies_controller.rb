d43cb5432b79b7900ba1e273f7a273d30aea87a9
Anshul Agrawal <anagrawal@bestpeers.com>
2023-06-27 13:40:30 +0530

    Modified movies/index action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,20 +3,16 @@ class MoviesController < ApplicationController
   before_action :movie_find, except:[:index, :new, :create,:upcoming_movies,:recent_movies]
   
   def index
-    if params[:filter] == 'upcoming_movies'
-      @movies = Movie.where('released_date > ?', Date.today).order(released_date:'asc')
-    elsif params[:filter] == 'popular'
-      @movies = Movie.joins(:reviews).group('movies.id').having('AVG(reviews.star) > ?',3.5)
-    elsif params[:filter] == 'recent_movies'
-      @movies = Movie.where('released_date < ?', Date.today).order('released_date desc').limit(6)
-    elsif params[:category].blank?
-      @movies =  Movie.all
-    else
+    if params[:filter]
+      @movies = set_movie
+    elsif params[:category]
       category_id = Category.find_by(name: params[:category]).id
       @movies = Movie.where(category_id: category_id)
+    else
+      @movies =  Movie.all
     end
   end
-
+  
   def show
   end
 
@@ -52,6 +48,17 @@ class MoviesController < ApplicationController
   end
 
   private
+
+  def set_movie
+    if params[:filter] == 'upcoming_movies'
+      @movies = Movie.where('released_date > ?', Date.today).order(released_date:'asc')
+    elsif params[:filter] == 'popular'
+      @movies = Movie.joins(:reviews).group('movies.id').having('AVG(reviews.star) > ?',3.5)
+    elsif params[:filter] == 'recent_movies'
+      @movies = Movie.where('released_date < ?', Date.today).order('released_date desc').limit(6)
+    end
+    
+  end
 
   def movie_find
     @movie = Movie.find(params[:id])
